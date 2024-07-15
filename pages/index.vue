8d86a77526db82518d07d034a5dde1b74c5e3935
rohan-goyal16 <rohan.goyal@comprotechnologies.com>
2024-07-15 10:18:01 +0530

    Server , nitro , useFetch , useAsyncData

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,25 @@
 <script setup >
-
-    // definePageMeta({
-    //     middleware: 'auth',
-    // });
-    // const {$sayHello} = useNuxtApp() ; 
-    // $sayHello("Rohan") ; 
-
-    const counter = useCounter() ; 
     
+    // const {data : products , pending} = await useLazyFetch('/api/products') ; 
+    // // console.log(data.value) ; 
 
+    const {data: productCount , pending} = await useLazyAsyncData("products" , () => {
+        $fetch("/api/products")
+    }) ;
 
+    console.log(productCount) ;
+
+    const refresh = () => refreshNuxtData('products')
 </script>
+
+
 
 <template>
     <div>
-        Counter :  {{ counter }}
-        <button @click="counter++">+</button>
-        <button @click="counter--">-</button>
+       <h1>This is the main index page</h1>
+       <br> <br>
+       <p>{{ pending ? "loading" :  productCount }}</p>
+
+       <button @click="refresh">Refresh</button>
     </div>
-
-
-    <Counter />
 </template>
